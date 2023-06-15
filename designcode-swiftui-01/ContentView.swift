84ff6635d61b84dd7133faecedb8ecfbec8fcbfc
Narendra Yenugula <narendra.yenugula@gmail.com>
2023-06-15 22:02:55 +0200

    Finished Layout and stack tutorial

---
+++ designcode-swiftui-01/ContentView.swift
@@ -0,0 +1,57 @@
+//
+//  ContentView.swift
+//  designcode-swiftui-01
+//
+//  Created by Narendra Yenugula on 15/06/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        ZStack {
+            //background
+            VStack {
+                Spacer()
+            }
+            .frame(width: 300, height: 220)
+            .background(Color.blue )
+            .cornerRadius(20)
+            .shadow(radius: 20)
+            .offset(x: 0, y: -20)
+            
+            //card
+            VStack {
+                HStack {
+                    VStack(alignment: .leading) {
+                        Text("UI Design ")
+                            .font(Font.title)
+                            .fontWeight(Font.Weight.semibold)
+                            .foregroundColor(Color.white)
+                        Text("Certificate")
+                            .foregroundColor(Color("accent"))
+                    }
+                    Spacer()
+                    Image("Logo1")
+                }
+                .padding(Edge.Set.horizontal, 20)
+                .padding(Edge.Set.top, 20)
+                Spacer()
+                Image("Card1")
+                    .resizable()
+                    .aspectRatio(contentMode: ContentMode.fill)
+                    .frame(width: 300, height: 110, alignment: Alignment.top)
+            }
+            .frame(width: 340, height: 220)
+            .background(Color.black)
+            .cornerRadius(20)
+        .shadow(radius: 20)
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ designcode-swiftui-01/designcode_swiftui_01App.swift
@@ -0,0 +1,17 @@
+//
+//  designcode_swiftui_01App.swift
+//  designcode-swiftui-01
+//
+//  Created by Narendra Yenugula on 15/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct designcode_swiftui_01App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
